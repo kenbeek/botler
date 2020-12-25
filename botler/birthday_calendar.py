5da9ddd1df0c1b50581698149cdd5c2ffed5c1f5
Trudie Bosse <trudie_bosse@hotmail.com>
2020-12-25 14:34:15 +0100

    Added birthday calendar scripts

---
+++ botler/birthday_calendar.py
@@ -0,0 +1,55 @@
+import datetime
+import json
+from datetime import date
+
+
+## database
+# read function
+def read_birthday_calendar():
+    """Reads in the data from birthday_calendar.json and returns a dictionary with people and their birthdays
+
+    Returns:
+        Dictionary : The complete birthday calendar
+    """
+    with open("data/birthday_calendar.json", "r+") as f:
+        calendar = json.load(f)
+    return calendar
+
+
+def birthday_is_today(calendar):
+    """Filters the people whose birthday is today out of the calendar
+
+    Args:
+        calendar (dictionary): dictionary of people and their birthdays
+
+    Returns:
+        list : List of people whose birthday is today (jolly good fellows)
+    """
+    jolly_good_fellows = []
+    today = date.today()
+    for key in c.keys():
+        birthday = datetime.datetime.strptime(c[key]["birthday"], "%d-%m-%Y")
+        if birthday.day == today.day and birthday.month == today.month:
+            print(key)
+            jolly_good_fellow = {
+                "name": key,
+                "age": today.year - birthday.year,
+            }
+            if c[key]["nickname"] != "None":
+                jolly_good_fellow["nickname"] = c[key]["nickname"]
+            jolly_good_fellows = jolly_good_fellows + [jolly_good_fellow]
+
+    return jolly_good_fellows
+
+
+# write function
+
+## mutatie functies
+
+# email api
+
+
+if __name__ == "__main__":
+    c = read_birthday_calendar()
+    b = birthday_is_today(c)
+    print(b)
