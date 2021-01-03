76fddec2c12fb389ed48c8f66e42d61c50327e88
Trudie Bosse <trudie_bosse@hotmail.com>
2021-01-03 19:34:09 +0100

    Small details on design

--- botler/static/js/script.js
+++ botler/static/js/script.js
@@ -1,6 +1,6 @@
 const numItemsToGenerate = 2; //how many gallery items you want on the screen
 const numImagesAvailable = 328; //how many total images are in the collection you are pulling from
-const imageWidth = 800; //your desired image width in pixels
+const imageWidth = 1800; //your desired image width in pixels
 const imageHeight = 300; //desired image height in pixels
 const collectionID = 416021; //the collection ID from the original url
 const $galleryContainer = document.querySelector('.gallery-container');
